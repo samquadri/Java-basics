0f9a857db088ae314c5a7ce59244ea0264c5f169
samquadri <quadri.sam@gmail.com>
2015-02-17 13:05:08 +0530

    Create youtube.java

---
+++ youtube.java
@@ -0,0 +1,9 @@
+/* Basic compiling program */
+class apples
+{
+	public static void main(String args[])
+	{
+		System.out.println("Hello Youtube!");
+	}
+}
+/* To compile this, use javac youtube.java and to run it, use java apples */
